b7cb23eb9b5458b8ad690ed15aef945629c41951
sh4hids <hello@shahid.pro>
2019-01-11 07:46:45 +0600

    Add config for production

--- src/app/config.js
+++ src/app/config.js
@@ -3,6 +3,10 @@ const config = {
     server: 'http://localhost:8000/',
     api: 'api/v1',
   },
+  production: {
+    server: 'http://afterjskit.com/',
+    api: 'api/v1',
+  },
 };
 
 export default config;
